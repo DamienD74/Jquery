9344070cbd135797ad4b4bcace1fd56721abc54b
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-04-02 11:43:00 +0200

    exo-2

--- exo-2/script.js
+++ exo-2/script.js
@@ -2,3 +2,8 @@ console.log('----------');
 console.log('Exercice 2');
 console.log('----------');
 
+/*
+1- Les 3 principaux type de sélecteur sont: - #id qui permet de sélectionner l'élément grace a son ID.
+                                            - .class qui permet de sélectionner l'élément grace a sa classe.
+                                            - element qui permet de sélectionner directement l'élément.
+ */
