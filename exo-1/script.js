ac3e3fe0d760513349160f9dd4209953e244ba49
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-04-02 10:45:18 +0200

    exo-1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,3 +1,17 @@
+/*
+1- Jquery et JS possède les memes fonctionnalités mais Jquery est conçu pour éviter au développeur de devoir écrire énormément de ligne
+pour faire quelques choses, grace a Jquery on peut fait en 1 ligne de code ce que l'on doit faire en 5 lignes avec JS.
+
+2- Nous devons placer la balise script juste avant la fermeture du body </body> il faut le mettre a cette position pour que les requètes mise en place
+soit appeler a la fin.
+
+4- Si la page HTML est complètement chargé et pret alors la function ce lit, cette fonction permet d'afficher un text dans la console.
+
+5- Elle permet d'avoir des informations si la page html ce lit correctement et si Jquery est bien fonctionnel.
+
+6- le symbole $
+*/
+
 console.log('----------');
 console.log('Exercice 1');
 console.log('----------');
