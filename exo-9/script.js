391535d173eecde0c3239de5646fd72fee4dc93d
Neo-Prod <damien.delhaye74@orange.fr>
2021-04-08 11:13:40 +0200

    exo-9

--- exo-9/script.js
+++ exo-9/script.js
@@ -1,1 +1,35 @@
 console.log("exercice 9");
+
+$(".dropBox").hide();
+
+$(".collapseJS").on("click", function()
+{
+    if ($(this).children(".dropBox").is(":visible"))
+    {
+        $(this).children(".dropBox").hide();
+    }
+    else
+    {
+        $(this).children(".dropBox").show();
+    }
+ 
+});
+
+$(".spamAlerte").on("click", function()
+{
+    $(this).remove();
+})
+
+$(".buttonDrop").on("click", function()
+{
+    if ($(this).parent().children("ul").is(":visible"))
+    {
+        $(this).parent().children("ul").hide();
+    }
+    else
+    {
+        $(this).parent().children("ul").show();
+    }
+});
+
+$(".fix").addClass("sticky-top").css("top", "10px");
